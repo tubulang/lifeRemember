a6d12f6f700e297f0ee4be591b5f2453f11acd9d
tubulang <1053736438standard@gmail.com>
2019-03-05 14:29:55 +0800

    add example for classification

--- miniLifeRemember/pages/classification/classification.js
+++ miniLifeRemember/pages/classification/classification.js
@@ -1,66 +1,134 @@
-// pages/classification/classification.js
+import { $wuxSelect } from '../../miniprogram_npm/wux-weapp/index'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    value1: '',
+    title1: '',
+    value2: '',
+    title2: '',
+    value3: '',
+    title3: '',
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onClick1() {
+    $wuxSelect('#wux-select1').open({
+      value: this.data.value1,
+      options: [
+        '法官',
+        '医生',
+        '猎人',
+        '学生',
+        '记者',
+        '其他',
+      ],
+      onConfirm: (value, index, options) => {
+        console.log('onConfirm', value, index, options)
+        if (index !== -1) {
+          this.setData({
+            value1: value,
+            title1: options[index],
+          })
+        }
+      },
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onClick2() {
+    $wuxSelect('#wux-select2').open({
+      value: this.data.value2,
+      options: [{
+        title: 'iPhone 3GS',
+        value: '001',
+      },
+      {
+        title: 'iPhone 5',
+        value: '002',
+      },
+      {
+        title: 'iPhone 5S',
+        value: '003',
+      },
+      {
+        title: 'iPhone 6',
+        value: '004',
+      },
+      {
+        title: 'iPhone 6S',
+        value: '005',
+      },
+      {
+        title: 'iPhone 6P',
+        value: '006',
+      },
+      {
+        title: 'iPhone 6SP',
+        value: '007',
+      },
+      {
+        title: 'iPhone SE',
+        value: '008',
+      },
+      {
+        title: 'iPhone 7',
+        value: '009',
+      },
+      ],
+      onConfirm: (value, index, options) => {
+        console.log('onConfirm', value, index, options)
+        if (index !== -1) {
+          this.setData({
+            value2: value,
+            title2: options[index].title,
+          })
+        }
+      },
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onClick3() {
+    $wuxSelect('#wux-select3').open({
+      value: this.data.value3,
+      multiple: true,
+      toolbar: {
+        title: 'Please choose',
+        confirmText: 'ok',
+      },
+      options: [{
+        title: '画画',
+        value: '1',
+      },
+      {
+        title: '打球',
+        value: '2',
+      },
+      {
+        title: '唱歌',
+        value: '3',
+      },
+      {
+        title: '游泳',
+        value: '4',
+      },
+      {
+        title: '健身',
+        value: '5',
+      },
+      {
+        title: '睡觉',
+        value: '6',
+      },
+      ],
+      onChange: (value, index, options) => {
+        console.log('onChange', value, index, options)
+        this.setData({
+          value3: value,
+          title3: index.map((n) => options[n].title),
+        })
+      },
+      onConfirm: (value, index, options) => {
+        console.log('onConfirm', value, index, options)
+        this.setData({
+          value3: value,
+          title3: index.map((n) => options[n].title),
+        })
+      },
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
